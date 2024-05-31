f55c1232dc970a40bcd1c6a3837ab64fad1e4566
Ben Bassler <mail@benbaessler.com>
2024-05-31 16:29:12 +0100

    chore: update schema

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -2,11 +2,18 @@ import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
   User: p.createTable({
-    // evm address
-    id: p.bigint(),
+    // address:castHash
+    id: p.string(),
     ticketBalance: p.bigint(),
     referralFeesEarned: p.bigint(),
     creatorFeesEarned: p.bigint(),
+  }),
+  Ticket: p.createTable({
+    // cast hash
+    id: p.string(),
+    channelId: p.string(),
+    supply: p.bigint(),
+    holdersCount: p.bigint()
   }),
   Transaction: p.createTable({
     id: p.string(),
@@ -14,6 +21,7 @@ export default createSchema((p) => ({
     senderFid: p.bigint(),
     castHash: p.string(),
     amount: p.bigint(),
+    price: p.bigint(),
     timestamp: p.bigint(),
   }),
   Round: p.createTable({
