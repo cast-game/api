625a3b04680bac91328883e52a261a0197144f3b
Ben Bassler <mail@benbaessler.com>
2024-06-11 15:41:25 +0100

    chore: update active tier function

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,37 @@
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { priceTiers } from "./constants";
 
 export const neynar = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
 
 export const getActiveTier = async (address: string) => {
   const res = await neynar.fetchBulkUsersByEthereumAddress([address]);
+  const user = res[address]![0]!;
 
-  const meta = {
-    "x-dune-api-key": process.env.DUNE_API_KEY || "",
-  };
-  const header = new Headers(meta);
-  try {
-    const latest_response = await fetch(
-      `https://api.dune.com/api/v1/query/3418402/results?&filters=fid=${
-        res[address]![0]?.fid
-      }`,
-      {
-        method: "GET",
-        headers: header,
-      }
-    );
+  let tier;
+  if (user.follower_count < 400) {
+		tier = 0;
+	} else if (user.follower_count > 400 && user.follower_count < 1000) {
+		tier = 1;
+	} else if (user.follower_count > 1000 && user.follower_count < 10000) {
+		tier = 2;
+	} else if (user.follower_count > 10000 && user.follower_count < 50000) {
+		tier = 3;
+	} else {
+		tier = 4;
+	}
 
-    const body = await latest_response.text();
-    const recs = JSON.parse(body).result.rows[0];
-    return BigInt(recs.fid_active_tier);
-  } catch (error) {
-    return 0n;
-  }
+	if (!user.power_badge && tier > 0) tier--;
+	return BigInt(tier);
 };
+
+export function getPrice(tier: number, supply: number): number {
+	const priceTier = priceTiers[tier]!;
+	const growthRate =
+		Math.log(priceTier.priceAt50 / priceTier.startingPrice) / 50;
+	const newSupply = supply;
+	const pricePerShare =
+		priceTier.startingPrice * Math.exp(growthRate * newSupply);
+
+	return Math.ceil(pricePerShare);
+}
+
---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+// in $DEGEN
+export const priceTiers = [
+	{
+		startingPrice: 25, // $0.50
+		priceAt50: 4000, // $80
+	},
+	{
+		startingPrice: 50, // $1
+		priceAt50: 5000, // $100
+	},
+	{
+		startingPrice: 200, // $4
+		priceAt50: 10000, // $200
+	},
+	{
+		startingPrice: 350, // $7
+		priceAt50: 12500, // $250
+	},
+	{
+		startingPrice: 500, // $10
+		priceAt50: 17500, // $350
+	},
+];
